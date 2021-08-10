27b9796bc02509000d14ddc4e67920e75c091a98
Suraj Sangwan <76832120+Suraj-Sangwan@users.noreply.github.com>
2021-08-10 10:39:24 +0530

    Setting up stuff for Jenkins

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+rm -rf  packages/apps/TouchGestures
+
+git clone https://github.com/Suraj-Sangwan/packages_apps_TouchGestures.git packages/apps/TouchGestures
+
+#Replacing AboutPhoneData.java
+
+cd packages/apps/Settings/src/com/android/settings
+
+rm AboutPhoneData.java 
+
+wget https://transfer.sh/1trknXT/AboutPhoneData.java
